4aed5e365ae67c1dbbf29c3ef63bac6c78f3ec11
Manish <manishraz32@gmail.com>
2022-09-01 21:22:04 +0530

    manish

---
+++ assets/js/common.js
@@ -0,0 +1,16 @@
+
+function convertToJSON(response) {
+  return response.json();
+}
+
+document.getElementById('nav-search-button').addEventListener('click', function() {
+  window.location.href = "./search.html";
+});
+
+document.getElementById('main-title').addEventListener('click', function() {
+  window.location.href = './index.html';
+})
+
+document.getElementById('logo').addEventListener('click', function() {
+  window.location.href = './index.html';
+})
